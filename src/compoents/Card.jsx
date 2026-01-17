997b90a67df1ed30ca1be22eab298f5268faaf89
GOWTHAM9384 <gowthamaravinth00@gmail.com>
2026-01-17 22:07:08 +0530

    remove the card width

--- src/compoents/Card.jsx
+++ src/compoents/Card.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Card = ({ img ,varient,property,content}) => {
   return (
-    <div className={`w-[100] p-5 ${property} text-center`}>
+    <div className={`p-5 ${property} text-center`}>
       <img src={img} alt="" className='mx-auto' />
       <div className={`${content} flex flex-col justify-center`}>
         <h3 className={`text-2xl font-bold text-center ${varient}`}>In-app transer</h3>
